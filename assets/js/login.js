b1e1e03516f7680bdc9a6a435358c14e08090c96
spring <842437627@qq.com>
2020-09-12 14:18:20 +0800

    完成了登录注册的操作，以及密码的校验

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,28 +2,21 @@
 $(function () {
     // 点击去注册，登录隐藏注册展现
     $("#link-register").on("click", function () {
-        $(".registerbox").show();
+        $(".register-box").show();
         $(".login-box").hide();
 
     });
     // 点击去登录，登录展现注册隐藏
     $("#link-login").on("click", function () {
         $(".login-box").show();
-        $(".registerbox").hide();
+        $(".register-box").hide();
 
     });
-    // 自定义验证规则
+    //  密码校验
     var form = layui.form
     form.verify({
-        pwd: [
-            /^[\S]{6,12}$/
-            , '密码必须6到12位，且不能出现空格'
-        ],
-        repwd: function (value) {
-            var pwd = $(".registerbox [name=password]").val()
-            if (pwd !== value) {
-                return "两次密码不一致！！！！"
-            }
-        }
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+
     })
+     
 })
